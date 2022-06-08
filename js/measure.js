58cc4a09a2f2fff9eda42be79cad6bb02366733f
KGrzeg <kupczykgrzeg@gmail.com>
2022-06-09 01:43:56 +0200

    Walidacja punktów

--- js/measure.js
+++ js/measure.js
@@ -8,16 +8,27 @@ app.registerModule({
       case "app_submit":
         const ID1 = document.getElementById("meas_ID1").value;
         const ID2 = document.getElementById("meas_ID2").value;
+        let found = 0;
+        const points = [];
 
         for (let i = 0; i < app.points.length; i++) {
           if (ID1 == app.points[i].code) {
-            app.points_to_draw[0] = app.points[i]
+            points[0] = app.points[i];
+            found++;
+
           } else if (ID2 == app.points[i].code) {
-            app.points_to_draw[1] = app.points[i]
+            points[1] = app.points[i];
+            found++;
           }
         }
 
-        return app.loadView(app.views.measure);
+        if (found != 2) {
+          alert("Nie znaleziono punktów!")
+        } else {
+
+          app.points_to_draw = points;
+          return app.loadView(app.views.measure);
+        }
     }
   },
 });
